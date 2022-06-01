8f74394e75ba561d27a7462c36c172ff08c4bc02
i am nena <58001221+nenapisannoe@users.noreply.github.com>
2022-06-01 12:44:58 +0300

    generation of different types of garbage

--- classes/map_item.py
+++ classes/map_item.py
@@ -10,7 +10,7 @@ class Map_item(pg.sprite.Sprite):
     '''
 
     # initializing garbage
-    def __init__(self, tag, image, volume=0):
+    def __init__(self, tag, image = 0, volume=0):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.transform.scale(pg.image.load(item_picture[tag]).convert_alpha(),(50,50))
         self.rect = self.image.get_rect()
