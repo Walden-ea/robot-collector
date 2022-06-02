4ee42fd0b2fb4cb98e470481de16814eeb829bb7
CyberSniff <58146470+CyberSniff@users.noreply.github.com>
2022-06-02 13:18:03 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from time import sleep
 
 import pygame as pg
+import math
 import sys
 import random
 import array as arr
@@ -9,8 +10,19 @@ import classes.window as ww
 
 import classes.map_item as mi
 import classes.wall as wl
+import classes.container as ct
 
-# you may refactor it however you please
+class Player(pg.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pg.Surface((40, 40))
+        self.image.fill('red')
+        self.rect = self.image.get_rect(center=(300, 300))
+        self.mask = pg.mask.from_surface(self.image)
+
+    def update(self):
+        if pg.mouse.get_pos():
+            self.rect.center = pg.mouse.get_pos()
 
 # а может тут это в класс painter завернуть чтобы не передавать постоянно х и у not my business tho
 # from classes import robot
@@ -20,42 +32,134 @@ import classes.wall as wl
 WINDOW_WIDTH = 900
 WINDOW_HIGHT = 800
 
+pg.init()
+screen = pg.display.set_mode((1300, 800))
+player = pg.sprite.GroupSingle(Player())
+
 num = random.randint(3, 5)
 coords = ([0, 0] * num)
+
+cont_coords = ([0,0] * 3)
 
 for i in range(num):
     coords[i] = ww.generate_coords()
 
 
+
 def main():
-    screen = pg.display.set_mode((1300, 800))
+
     fps = 60
     clock = pg.time.Clock()
 
-    robot = rb.Robot(5, 20.0,WINDOW_WIDTH, WINDOW_HIGHT, sensor_length=40, x=WINDOW_WIDTH / 2, y=WINDOW_HIGHT / 2)
-    wall1 = wl.Wall(700, 50)
-    wall2 = wl.Wall(50, 400)
-    wall3 = wl.Wall(350, 50)
+    cont = []
+
+    for i in range (3):
+        cont_coords[i] = ww.generate_coords()
+
+    for i in range(3):
+        cont.append(ct.Container(i, cont_coords[i][0],cont_coords[i][1]))
+
+    container_group = pg.sprite.Group()
+    for c in cont:
+        container_group.add(c)
+
+    robot = rb.Robot(5, 20.0,WINDOW_WIDTH, WINDOW_HIGHT, container_group, sensor_length=40, x=WINDOW_WIDTH / 2, y=WINDOW_HIGHT / 2)
+    wall1 = wl.Wall(98, 100,700, 50)
+    wall2 = wl.Wall( 650, 200, 50, 400)
+    wall3 = wl.Wall( 100, 450, 350, 50)
+    walls = pg.sprite.Group()
+    walls.add(wall1,wall2,wall3)
+
 
     garb = []
+
+
+
     for i in range(num):
-        garb.append(mi.Map_item(random.randint(0, 2)))
+        garb.append(mi.Map_item(random.randint(0, 2),coords[i][0], coords[i][1]))
+
 
     pg.display.set_caption("Робот-сборщик мусора")
+
+    new_garb = []
+    new_garb_coords = []
+    garb_group = pg.sprite.Group()
+    garb_group.add(garb)
     while True:
+
         for i in pg.event.get():
             if i.type == pg.QUIT:
                 sys.exit()
+            if i.type == pg.MOUSEBUTTONUP:
+                mouse_pos = pg.mouse.get_pos()
+                new_garb_coords.append(mouse_pos)
+                new_garb.append(mi.Map_item(random.randint(0, 2), mouse_pos[0], mouse_pos[1]))
+                garb_group.add(new_garb[len(new_garb)-1])
+                pass
         ww.draw_background(screen, WINDOW_WIDTH, WINDOW_HIGHT)
         ww.draw_robot(screen, robot)
-        ww.draw_walls(screen, wall1, 98, 100)
-        ww.draw_walls(screen, wall2, 650, 200)
-        ww.draw_walls(screen, wall3, 100, 450)
-        for i in range(num):
-            garb[i].update(screen, coords[i][0], coords[i][1])
+        ww.draw_walls(screen, wall1)
+        ww.draw_walls(screen, wall2)
+        ww.draw_walls(screen, wall3)
+        for i_g in range(num):
+            if garb[i_g] in garb_group:
+                garb[i_g].update(screen, coords[i_g][0], coords[i_g][1])
+        for i_g in range(len(new_garb)):
+            if new_garb[i_g] in garb_group:
+                new_garb[i_g].update(screen, new_garb_coords[i_g][0], new_garb_coords[i_g][1]) #todo проверить почему не больше 4х
+            #print("i_g: "+str(i_g))
+            #print("ngc: " + str(len(new_garb_coords)))
+        for c in cont:
+            screen.blit(c.image, [c.pos_x, c.pos_y])
+
         for sensor in robot.sensors:
-            sensor.check_collide([wall1, wall2, wall3])
-        robot.tick()
+            sensor.check_collide(walls)
+        robot.go()
+        #obstacle.draw(screen)
+        # for wall in walls.sprites():
+        #     pg.sprite.GroupSingle(wall).draw(screen)
+        # collision
+        # updating and drawing
+        player.update()
+        player.draw(screen)
+
+        # collision
+        for s in robot.sensors:
+            c_sprite = pg.sprite.spritecollide(s, container_group, False, pg.sprite.collide_mask)
+            sprite = pg.sprite.spritecollide(s, garb_group, True, pg.sprite.collide_mask)
+            if pg.sprite.spritecollide(s, walls, False, pg.sprite.collide_mask):
+                W1 = pg.sprite.collide_mask(sensor, wall1)
+                W2 = pg.sprite.collide_mask(sensor, wall2)
+                W3 = pg.sprite.collide_mask(sensor, wall3)
+
+                print('COLLIDE ', W1, W2, W3)
+                player.sprite.image.fill('green')
+            elif sprite:
+                robot.capacity-= sprite[0].volume
+                robot.c_tag = sprite[0].tag
+                print("COLLIDES")
+                player.sprite.image.fill('blue')
+            elif c_sprite:
+                robot.capacity = 10
+                print("CONTAINER")
+                player.sprite.image.fill('yellow')
+            else:
+                player.sprite.image.fill('red')
+
+
+        #это мышкой проверка
+        # if pg.sprite.spritecollide(player.sprite, walls, False, pg.sprite.collide_mask):
+        #     print("collides")
+        #     player.sprite.image.fill('green')
+        # elif pg.sprite.spritecollide(player.sprite, garb_group, False, pg.sprite.collide_mask):
+        #     print("COLLIDES")
+        #     player.sprite.image.fill('blue')
+        # elif pg.sprite.spritecollide(player.sprite, container_group, False, pg.sprite.collide_mask):
+        #     print("CONTAINER")
+        #     player.sprite.image.fill('yellow')
+        # else:
+        #     player.sprite.image.fill('red')
+
         clock.tick(fps)
         pg.display.update()
         #sleep(3)
---
+++ pathfinder.py
@@ -0,0 +1,101 @@
+import enum
+import classes.window
+from pygame import math
+from classes import robot
+from classes import helper
+
+class Mode(enum.Enum):
+    #   todo можно вынести в отдельный класс с конфигом
+    ROAMING = 0  # патрулирует
+    CARRYING = 1  # несет мусор к контейнеру
+
+
+class Pathfinder:
+    '''
+    todo docs
+    '''
+    def __init__(self, robot, height, width):
+        self.mode = Mode.ROAMING
+        #x = robot.radius + 5
+        #y = robot.radius + 5
+        x = 800
+        y = 400
+        if helper.can_be_placed_at(x,y):
+            self.target_x = x
+            self.target_y = y
+        else: #todo надо тогда двигать, чо ниже заготовка чтобы оно не ругалось
+            self.target_x = x
+            self.target_y = y
+        self.win_height = height
+        self.win_width = width
+        self.roaming_targets = []
+        self.set_list_of_targets(robot)
+        self.target_num = 0
+
+    def set_list_of_targets(self, robot):
+        x = robot.radius+ robot.radius//5
+        y = robot.radius + robot.radius // 5
+        self.roaming_targets.append((x, y))  # robot.radius//5 -- это своего рода припуск
+        # экспериментальный
+        direction = 1  # направление, 1 -- вправо, -1 -- влево
+        while y < self.win_height - (3*robot.radius + robot.radius//5):
+            x = x+direction*(self.win_width - robot.radius*2)
+            if direction == -1:
+                y += 2*robot.radius
+            self.roaming_targets.append((x, y))
+            direction *= -1
+
+    def start_roaming(self):
+        self.mode = Mode.ROAMING
+
+    def start_carrying(self):
+        self.mode = Mode.CARRYING
+
+
+    def go_to_target(self, robot):  # todo возможно проще координаты робота хранить в виде вектора я хз
+        if(robot.capacity == 10):
+            v = (math.Vector2(self.target_x, self.target_y) - math.Vector2(robot.x, robot.y))
+            print("dest: "+ str(v.x)+", "+str(v.y))
+            #v.scale_to_length((math.Vector2(robot.velocity)).length()) todo когда почините навсистему
+            v.scale_to_length(5)
+            print("dest scaled: " + str(v.x)+", "+str(v.y))
+            robot.velocity = v
+            print(self.target_x)
+            print(self.target_y)
+            print(robot.velocity)
+        else:
+            d = robot.c_tag
+            cont = list(robot.containers)[d]
+            v = (math.Vector2(cont.pos_x, cont.pos_y) - math.Vector2(robot.x, robot.y))
+            print("dest: " + str(v.x) + ", " + str(v.y))
+            # v.scale_to_length((math.Vector2(robot.velocity)).length()) todo когда почините навсистему
+            v.scale_to_length(5)
+            print("dest scaled: " + str(v.x) + ", " + str(v.y))
+            robot.velocity = v
+            print(self.target_x)
+            print(self.target_y)
+            print(robot.velocity)
+
+
+
+    def roam(self, robot):
+        if math.Vector2(self.target_x, self.target_y).distance_to(math.Vector2(robot.x, robot.y)) < robot.radius:
+            #robot.velocity = math.Vector2(0,0)
+            self.target_x, self.target_y = self.roaming_targets[self.target_num]
+            if self.target_num == len(self.roaming_targets)-1:
+                self.roaming_targets.reverse()
+                self.target_num = 0
+            else:
+                self.target_num += 1
+        self.go_to_target(robot)
+        print(self.target_x,self.target_y)
+
+    def carry(self):
+        pass
+
+    def set_velocity(self, robot):
+        if self.mode == Mode.ROAMING:
+            self.roam(robot)
+        else:
+            self.carry()
+
